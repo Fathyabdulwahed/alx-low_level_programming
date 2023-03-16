557d432e3702db9dd1b5bbd655e2b5d7581774a3
Fathyabdulwahed <fathymabdulwahid@gmail.com>
2023-03-17 00:58:10 +0200

    task-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Description: a c program that prints using puts
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
